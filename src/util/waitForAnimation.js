63e2f8c5899221f4fd548eb41f99e6e6d1d2e9a9
Andrew Li <Li357@users.noreply.github.com>
2018-06-22 00:10:32 -0500

    Edit HOC returning class formatting

--- src/util/waitForAnimation.js
+++ src/util/waitForAnimation.js
@@ -1,19 +1,21 @@
 import React, { Component } from 'react';
 import { InteractionManager } from 'react-native';
 
-const waitForAnimation = Child => class WaitForAnimation extends Component {
-  state = { animated: false }
+const waitForAnimation = Child => (
+  class WaitForAnimation extends Component {
+    state = { animated: false }
 
-  componentDidMount() {
-    InteractionManager.runAfterInteractions(() => {
-      this.setState({ animated: true });
-    });
-  }
+    componentDidMount() {
+      InteractionManager.runAfterInteractions(() => {
+        this.setState({ animated: true });
+      });
+    }
 
-  render() {
-    return this.state.animated
-      ? <Child />
-      : null;
+    render() {
+      return this.state.animated
+        ? <Child />
+        : null;
+    }
   }
-};
+);
 export default waitForAnimation;
